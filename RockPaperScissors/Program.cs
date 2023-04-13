305d84bdf621a20815c251a471267063228b82fc
Chance Gardner <Chantz465@gmail.com>
2023-04-13 16:54:40 -0600

    first commit

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            string inputPlayer, inputCpu;
+            int randomInt;
+
+            Console.WriteLine("Choose between Rock, Paper and scissors: ");
+            inputPlayer = Console.ReadLine();
+
+            Random rnd = new Random();
+
+            randomInt = rnd.Next(1,4);
+
+            switch (randomInt)
+            {
+                case 1:
+                    inputCpu = "Rock";
+                    Console.WriteLine("Computer chose Rock");
+                    if (inputPlayer == "Rock")
+                    {
+                        Console.WriteLine("Draw!!\n\n");
+                    }
+                    else if (inputPlayer == "Paper")
+                    {
+                        Console.WriteLine("player Wins!!\n\n");
+                    }
+                    else if (inputPlayer == "Scissors")
+                    {
+                        Console.WriteLine("Computer Wins!!\n\n");
+                    }
+                    break;
+                case 2:
+                    inputCpu = "Paper";
+                    Console.WriteLine("Computer chose Paper");
+                    if (inputPlayer == "Paper")
+                    {
+                        Console.WriteLine("Draw!!\n\n");
+                    }
+                    else if (inputPlayer == "Scissors")
+                    {
+                        Console.WriteLine("player Wins!!\n\n");
+                    }
+                    else if (inputPlayer == "Rock")
+                    {
+                        Console.WriteLine("Computer Wins!!\n\n");
+                    }
+                    break;
+                case 3:
+                    inputCpu = "Scissors";
+                    Console.WriteLine("Computer chose Scissors");
+                    if (inputPlayer == "Scissors")
+                    {
+                        Console.WriteLine("Draw!!\n\n");
+                    }
+                    else if (inputPlayer == "Rock")
+                    {
+                        Console.WriteLine("player Wins!!\n\n");
+                    }
+                    else if (inputPlayer == "Scissors")
+                    {
+                        Console.WriteLine("Computer Wins!!\n\n");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid entry!");
+                    break;
+            }
+
+        }
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+
+}
